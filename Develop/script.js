3512b0135034d76316fcbaef97b6a88a98289ec9
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-07-31 22:23:41 -0400

    adding hours

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
-  ````// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+let today = dayjs();
+let currentDayEl = $('#currentDay');
+let timeBlockEl = $(".time-block");
+letInputEl = timeBlockEl.children(".description")
+
+// $('#currentDay').text(today.format('[Today is]MMM D, YYYY, h:mm:ss a'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +28,9 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
 });
+
+//display date & time
+$('#currentDay').text(today.format('[Today is ]dddd, MMMM D, YYYY'));
+$('#currentHour').text(today.format('[The time is ]h:mm a'));
